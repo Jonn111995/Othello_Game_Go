f67d8942535ebd52ac8a65976732031c8d2f785c
kai <gmgw8445@gmail.com>
2025-08-13 00:31:45 +0900

    game.goの追加
    - オセロゲームに必要なオセロとPlayer構造体の定義
    - Game全体の状態を保持する構造体を定義

---
+++ internal/domain/game.go
@@ -0,0 +1,50 @@
+package domain
+
+import "fmt"
+
+type Color int
+
+// オセロの白黒の定義
+const (
+	Empty Color = 0
+	Black Color = 1
+	White Color = -1
+)
+
+// Playerの情報
+type Player struct {
+	ID    string `json:"player_id"`
+	Name  string `json:"name"`
+	Color Color  `json:"color"`
+}
+
+// Gameの状態を保持する構造体
+// 盤面やゲームの状態、Playerの数も管理する
+type Game struct {
+	ID      string            `json:"game_id"`
+	Board   [8][8]Color       `json:"board"`
+	Players map[string]Player `json:"players"`
+	Turn    string            `json:"turn"`
+	Status  string            `json:"status"`
+}
+
+func (g *Game) Clone() *Game {
+	ng := &Game{
+		ID:      g.ID,
+		Players: map[string]Player{},
+		Board:   [8][8]Color{},
+		Turn:    g.Turn,
+		Status:  g.Status,
+	}
+	ng.Board = g.Board
+
+	// playerの情報を書き換えられないように値コピーする
+	for k, v := range g.Players {
+		ng.Players[k] = v
+	}
+	return ng
+}
+
+func (g *Game) String() string {
+	return fmt.Sprintf("Game<%s> players=%d turn=%s status=%s", g.ID, len(g.Players), g.Turn, g.Status)
+}
