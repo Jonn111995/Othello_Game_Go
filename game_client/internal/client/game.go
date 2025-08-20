de62571166b0f0e071fdb88f6c541584cc8f4c0f
kai <gmgw8445@gmail.com>
2025-08-20 23:16:27 +0900

    ゲームループの実装  オセロの盤面が描画できるように実装

---
+++ game_client/internal/client/game.go
@@ -0,0 +1,53 @@
+package client
+
+import (
+	"image/color"
+
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+)
+
+type Game struct {
+	board [8][8]int
+}
+
+func NewGame(board [8][8]int) *Game {
+	return &Game{board: board}
+}
+
+func (g *Game) Update(screen *ebiten.Image) error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	for yy := 0; yy < 8; yy++ {
+		for xx := 0; xx < 8; xx++ {
+			// 描画する座標位置
+			// ワールド座標の基準が64pxずつにしているので64をかけている
+			x := xx * 64
+			y := yy * 64
+			// 緑色の背景の部分
+			// ワールド座標いっぱいに隙間なく敷き詰めるので、64x64の矩形で描画
+			ebitenutil.DrawRect(screen, float64(x), float64(y), 64, 64, color.RGBA{0x20, 0x80, 0x30, 0xff})
+			b := g.board
+			v := b[yy][xx]
+			// オセロの駒の描画
+			// 若干小さく描画するので48x48の矩形で描画する
+			if v == 1 {
+				ebitenutil.DrawRect(screen, float64(x+8), float64(y+8), 48, 48, color.RGBA{0xff, 0xff, 0xff, 0xff})
+			}
+			if v == -1 {
+				ebitenutil.DrawRect(screen, float64(x+8), float64(y+8), 48, 48, color.RGBA{0x00, 0x00, 0x00, 0xff})
+			}
+		}
+	}
+	// draw simple HUD: game id and player id
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	// ゲームのワールド座標の範囲を返却する
+	// x=512, y=512の広さのワールド座標になる
+	// ウィンドウのサイズ物理的な座標に合わせて解像度の拡大縮小をエンジン側でしてくれる
+	// イメージ的には、縦横64pxの矩形が8枚ずつ縦横に並んでいるようなイメージの範囲で描画される
+	return 8 * 64, 8 * 64
+}
